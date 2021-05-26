3d5e6cccfeb3d9d1a0bf4bf30896abf0c4eff340
Nathan da Silva <pacsmacck@gmail.com>
2021-05-25 17:45:33 -0700

    database connected and running

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2')
 
 const PORT = process.env.PORT || 3001;
 const app = express()
@@ -7,7 +8,22 @@ const app = express()
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'BAT137982JUH@78',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
 // all above is boilerplate setup
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 app.use((req, res) => {
     res.status(404).end();
